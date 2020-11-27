869c9520a8837fb0dbdd9759af211c559e5f0dd7
ahsami <a.h.samiazar@gmail.com>
2020-11-27 16:21:59 +0330

    change in database and add unique constraint for lat and long then update prisma for rebuild schema and client models

--- prisma/migration.js
+++ prisma/migration.js
@@ -3,5 +3,5 @@ import {execFile} from 'child_process'
 const cmd = 'npx prisma migrate save --name init --experimental && npx prisma migrate up --experimental'
 
 
-const ef = execFile(cmd)
+execFile(cmd)
 
